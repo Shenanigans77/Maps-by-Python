82be3686f70ca239b84dff97ef1f333437fc2c07
Sean Shanahan <seanmodg@gmail.com>
2019-06-05 11:45:53 -0400

    Added conditional formatting for elevation.

--- application/script_1.py
+++ application/script_1.py
@@ -8,11 +8,16 @@ data = pandas.read_csv(open(filename))
 
 map = folium.Map(location=[45.372, -121.697],zoom_start=12,tiles='Stamen Terrain')
 
-for lat,lon,name in zip(data['LAT'], data['LON'],data['NAME']):
-    folium.Marker(
-        [lat, lon],
-        popup=name
-        ).add_to(map)
+def marker_color(elevation):
+    if elevation in range(0,1000):
+    	color = 'green'
+    elif elevation in range(1000,3000):
+    	color = 'orange' 
+    else:
+    	color = 'red'
+    return color
 
+for lat,lon,name,elev in zip(data['LAT'],data['LON'],data['NAME'],data['ELEV']):   	
+    folium.Marker([lat, lon],popup=name,icon=folium.Icon(color=marker_color(elev))).add_to(map)
 
 map.save('test.html')
