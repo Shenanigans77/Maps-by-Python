bac001e90615fc20c5dc7cb0eadd9ca8d4a8cc3e
Sean Shanahan <seanmodg@gmail.com>
2019-06-06 13:46:04 -0400

    Replaced magic numbers with calculations in marker_color

--- application/script_1.py
+++ application/script_1.py
@@ -6,12 +6,15 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 filename = os.path.join(current_dir, os.path.realpath("data/Volcanoes_USA.txt"))
 data = pandas.read_csv(open(filename))
 
-map = folium.Map(location=[45.372, -121.697],zoom_start=12,tiles='Stamen Terrain')
+# Instantiate map centered at the center of the dataset's coordinates
+map = folium.Map(location=[data['LAT'].mean(),data['LON'].mean()],zoom_start=12,tiles='Stamen Terrain')
 
 def marker_color(elevation):
-    if elevation in range(0,1000):
+    minimum = int(min(data['ELEV']))
+    step = int((max(data['ELEV'])-min(data['ELEV']))/3)
+    if elevation in range(minimum,minimum+step):
     	color = 'green'
-    elif elevation in range(1000,3000):
+    elif elevation in range(minimum+step,minimum+step*2):
     	color = 'orange' 
     else:
     	color = 'red'
